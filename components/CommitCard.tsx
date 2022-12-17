5955038dc7e83b0f58d1798d93c210a3c0d8f5b6
Dosybi <dosybi@gmail.com>
2022-12-17 23:28:45 +0600

    update time state

--- components/CommitCard.tsx
+++ components/CommitCard.tsx
@@ -1,7 +1,35 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 const CommitCard = ({ commit }: any) => {
+  const [timeAgo, setTimeAgo] = useState('')
+
+  const WAIT_TIME = 5000
+
+  useEffect(() => {
+    const time = setInterval(() => {
+      const dateFromAPI = commit.commit.committer.date
+      const now = new Date()
+      const datefromAPITimeStamp = new Date(dateFromAPI).getTime()
+      const nowTimeStamp = now.getTime()
+      const microSecondsDiff = Math.abs(datefromAPITimeStamp - nowTimeStamp)
+      const minutesDiff = Math.round(microSecondsDiff / (1000 * 60))
+      const timeLabel =
+        minutesDiff < 60
+          ? `${minutesDiff} minutes ago`
+          : Math.round(minutesDiff / 60) === 1
+          ? `1 hour ago`
+          : minutesDiff >= 60 && minutesDiff < 1440
+          ? `${Math.round(minutesDiff / 60)} hours ago`
+          : Math.round(minutesDiff / 1440) === 1
+          ? `1 day ago`
+          : `${Math.round(minutesDiff / 1440)} days ago`
+      setTimeAgo(timeLabel)
+    }, WAIT_TIME)
+    return () => clearInterval(time)
+  }, [])
+
   return (
     <div className="mb-6 rounded-lg border border-gray-200 bg-white p-4 shadow-md">
       <div className="mb-2 flex items-center justify-between">
@@ -25,6 +53,7 @@ const CommitCard = ({ commit }: any) => {
           {commit.author.login}
         </div>
       </Link>
+      <div>{timeAgo}</div>
       <div>{commit.commit.committer.date}</div>
     </div>
   )
